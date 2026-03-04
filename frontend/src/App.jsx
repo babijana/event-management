a09e03a3b7fe2b66219972126ea23279eb6bd778
babijana <babijanaj.23@cse.mrt.ac.lk>
2026-03-04 08:41:39 +0530

    change something in initialization

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+function App() {
+  return (
+    <div>
+      <h1>Event Management System</h1>
+      <p>Frontend setup successful 🚀</p>
+    </div>
+  );
+}
+
+export default App;
+
+
